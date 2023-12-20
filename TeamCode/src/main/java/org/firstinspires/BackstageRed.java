4fb1d8677f9b47ed7145c34d20dd7ee5ea20dbb2
JacksonDorschner <107083723+JacksonDorschner@users.noreply.github.com>
2023-12-19 22:15:32 -0800

    ill just debug later (vs code gave up)

--- TeamCode/src/main/java/org/firstinspires/BackstageRed.java
+++ TeamCode/src/main/java/org/firstinspires/BackstageRed.java
@@ -1,3 +1,7 @@
+/*TODO: add arm_subsystem into a conditional list
+ * 
+ */
+
 CuttleEncoder leftEncoder ;
 CuttleEncoder sideEncoder ;
 CuttleEncoder rightEncoder;
@@ -11,72 +15,113 @@ MecanumController chassis;
 
 @Autonomous(name="AutoRedBackstage", group="Centerstage")
 public class cuttleAuto extends InitializedOpmode{
+
+    PTPController ptpController
+
+    public final static double mm_per_inch = 25.4;
+
     @Override
     public void onInit()
     {
-            leftEncoder  = expHub .getEncoder(3,720*4);
-            sideEncoder  = ctrlHub.getEncoder(0,720*4);
-            rightEncoder = ctrlHub.getEncoder(3,720*4);
-            leftEncoder.setDirection(Direction.REVERSE);
+        //Drive Train
+        leftFrontMotor  = ctrlHub.getMotor(1);
+        rightFrontMotor = ctrlHub.getMotor(2);
+        rightBackMotor  = ctrlHub.getMotor(3);
+        leftBackMotor   = ctrlHub.getMotor(4);
 
-            encoderLocalizer = new ThreeEncoderLocalizer(
-                    leftEncoder  ,
-                    sideEncoder  ,
-                    rightEncoder ,
-                    29, // Radius of the wheel in mm
-                    130.5, // Distance between the two forward facing wheels in mm
-                    1.0 //Calibration constant (see below)
-            );
+        leftBackMotor .setDirection(Direction.REVERSE);
+        leftFrontMotor.setDirection(Direction.REVERSE);
 
-            leftFrontMotor  = ctrlHub.getMotor(1);
-            rightFrontMotor = ctrlHub.getMotor(2);
-            rightBackMotor  = ctrlHub.getMotor(3);
-            leftBackMotor   = ctrlHub.getMotor(4);
+        chassis = new MecanumController(
+            rightFrontMotor,
+            rightBackMotor,
+            leftFrontMotor,
+            leftBackMotor
+        );
 
-            leftBackMotor .setDirection(Direction.REVERSE);
-            leftFrontMotor.setDirection(Direction.REVERSE);
+        //Encoder
+        leftEncoder  = expHub .getEncoder(3,720*4);
+        sideEncoder  = ctrlHub.getEncoder(0,720*4);
+        rightEncoder = ctrlHub.getEncoder(3,720*4);
+        leftEncoder.setDirection(Direction.REVERSE);
 
-            chassis = new MecanumController(
-                rightFrontMotor,
-                rightBackMotor,
-                leftFrontMotor,
-                leftBackMotor
-            );
+        encoderLocalizer = new ThreeEncoderLocalizer(
+                leftEncoder  ,
+                sideEncoder  ,
+                rightEncoder ,
+                29,    // Radius of the wheel in mm
+                130.5, // Distance between the two forward facing wheels in mm
+                1.0    //Calibration constant 
+        );
+
+        //PID
+        ptpController = new PTPController(chassis, encoderLocalizer)
+
+        ptpController.setTranslational_PD_ctrlr(new PID(
+            translation_p_gain,
+            0,
+            translation_d_gain,
+            0.0,
+            maximumIntegralPower));
+        ptpController.setRotational_PID_ctrlr(new PID(
+            rotational_p_gain,
+            rotational_i_gain,
+            rotational_d_gain,
+            0.0,
+            maximumIntegralPower));
+
+        ptpController.setTranslational_PD_ctrlr(new PID( 
+            0.02,  // Proportional
+            0.0,   // Integral
+            0.002, // Derivative
+            0.0,   // Initial value (should be zero)
+            1.0    // Maximum integral power (to prevent integral windup)
+        ));
+        ptpController.setRotational_PID_ctrlr(new PID(
+            3,  // Proportional
+            0.0,   // Integral
+            0.2, // Derivative
+            0.0,   // Initial value (should be zero)
+            1.0    // Maximum integral power (to prevent integral windup)
+        ));
+
+        //Anti-Stall (continues to next WP if detects stalling)
+        ptpController.getAntistallParams().setMovePowerAntistallThreshold(0.2); // Maxmimum translational power where the bot is still stalled
+        ptpController.getAntistallParams().setRotatePowerAntistallThreshold(0.2); // Maxmimum rotation power where the bot is still stalled
+        ptpController.getAntistallParams().setMoveSpeedAntistallThreshold(0.015);  // Maximum speed in m/s for the bot to be considered stalled
+        ptpController.getAntistallParams().setRotateSpeedAntistallThreshold(0.3); // Maximum rotation speed in rad/s for the bot to be considered stalled
     }
-    
+
     public void main{
         super.main();
 
-        queue.addTask(new PointTask(
-            new Waypoint(
-                new Pose(1000,0,0),
-                0.5
-            ),
-            ptpController
-        ));
+        TaskList autoList = new TaskList();
+        queue.addTask(new CustomTask(()->{
+            switch(position){
+                case LEFT:
+                    arm_subsystem.grab();
+                    autoList.addTask(new DelayTask(500));
 
-        queue.addTask(new PointTask(
-            new Waypoint(
-                new Pose(1000,1000,Math.PI/2),
-                0.5
-            ),
-            ptpController
-        ));
+                    autoList.addTask(new PointTask(
+                        new Waypoint(
+                            new Pose(32*mm_per_inch, 0, Math.PI/2),
+                            0.5
+                        ),
+                        ptpController
 
-        queue.addTask(new ServoTask(claw, CLAW_OPEN));
+                    arm_subsystem.low();
+                    arm_subsystem.update();
 
-        queue.addTask(new DelayTask(400));
+                    autoList.addTask(new DelayTask(1000));
 
-        queue.addTask(new PointTask(
-            new Waypoint(
-                new Pose(0,0,0),
-                0.5
-            ),
-            ptpController
-        ));
-    }
+                    arm_subsystem.release();
+                    arm_subsystem.update();
+                
+                    ));
+            }
+        }))
 
-    public void mainLoop(){
-        super.mainLoop()
+        queue.addTask(autoList);
+
     }
 }
--- TeamCode/src/main/java/org/firstinspires/Cuttlefish/cuttleTest.java
+++ TeamCode/src/main/java/org/firstinspires/Cuttlefish/cuttleTest.java
@@ -1,3 +1,4 @@
+//JUST A DEMO DOC!!!
 //Cuttle Fish Docs: https://14496roboctopi.github.io/Cuttlefish/cuttlefish/index.html
 
 //Mecanum Setup
@@ -6,7 +7,7 @@ public CuttleMotor rightFrontMotor;
 public CuttleMotor rightBackMotor ;
 public CuttleMotor leftBackMotor  ;
 MecanumController chassis;
-
+@deactivate
 @Override
 public void onInit(){
         leftFrontMotor  = ctrlHub.getMotor(3);
@@ -135,4 +136,40 @@ public class QueueExample extends InitializedOpmode {
     {
         super.mainLoop();
     }
+}
+
+//Three encoder LOC
+CuttleEncoder leftEncoder ;
+CuttleEncoder sideEncoder ;
+CuttleEncoder rightEncoder;
+
+ThreeEncoderLocalizer encoderLocalizer;
+
+@Override
+public void onInit()
+{
+        leftEncoder  = expHub .getEncoder(3,720*4);
+        sideEncoder  = ctrlHub.getEncoder(0,720*4);
+        rightEncoder = ctrlHub.getEncoder(3,720*4);
+        leftEncoder.setDirection(Direction.REVERSE);
+
+        encoderLocalizer = new ThreeEncoderLocalizer(
+                leftEncoder  ,
+                sideEncoder  ,
+                rightEncoder ,
+                29, // Radius of the wheel in mm
+                130.5, // Distance between the two forward facing wheels in mm
+                1.0 //Calibration constant (see below)
+        );
+}
+//The Localizer MUST be initialized AFTER the encoders are initialized and configured.
+@Override
+public void mainLoop()
+{
+        encoderLocalizer.update();
+        System.out.println(encoderLocalizer.getPos());
+        telemetry.addData("Localizer X:",encoderLocalizer.getPos().getX());
+        telemetry.addData("Localizer Y:",encoderLocalizer.getPos().getY());
+        telemetry.addData("Localizer R:",encoderLocalizer.getPos().getR());
+        telemetry.update();
 }
